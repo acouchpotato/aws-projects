fb108aa17da3c72291a5383a99317506fcb0ac58
root <root@DESKTOP-9H13JER>
2022-08-12 00:10:30 +0100

    first_web_page

--- flask-02-jinja-template/jinja.py
+++ flask-02-jinja-template/jinja.py
@@ -11,9 +11,5 @@ def number():
     var1, var2 = 5210, 8960
     return render_template('body.html', num1 = var1, num2 = var2, multiplication = var1*var2)
 
-
-
-
-
 if __name__ == '__main__':
     app.run(debug=True)
